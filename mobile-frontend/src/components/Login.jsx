487e778359b295a46cccc198c77648830a1ec768
ambuj vashistha <ambujva123@gmail.com>
2025-09-24 11:45:34 +0530

    login function

--- mobile-frontend/src/components/Login.jsx
+++ mobile-frontend/src/components/Login.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function Login() {
-  return <Text>Login</Text>;
+  const [message, setLogin] = useState("");
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
 }
