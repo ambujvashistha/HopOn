b90d1bda3c28a07176f0afa92924789c1589c2a0
ambuj vashistha <ambujva123@gmail.com>
2025-09-24 11:50:30 +0530

    login function styling

--- mobile-frontend/src/components/Login.jsx
+++ mobile-frontend/src/components/Login.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { Text, TextInput, View } from "react-native";
 
 export default function Login() {
   const [message, setLogin] = useState("");
   return (
     <View>
       <Text>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your E-mail"
+      ></TextInput>
     </View>
   );
 }
+
+const styles = {
+  input: {
+    padding: 20,
+    width: 300,
+    height: 40,
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+};
