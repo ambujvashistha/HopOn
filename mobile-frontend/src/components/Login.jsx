65d5f2d5ac82e2897c2c72e89bea01c65624d022
ambuj vashistha <ambujva123@gmail.com>
2025-09-24 11:56:24 +0530

    added login card

--- mobile-frontend/src/components/Login.jsx
+++ mobile-frontend/src/components/Login.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Login() {
-  const [message, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View>
       <Text>Login</Text>
       <TextInput
         style={styles.input}
         placeholder="Enter your E-mail"
+        value={email}
       ></TextInput>
       <TextInput
         style={styles.input}
         placeholder="Enter your password"
       ></TextInput>
+
+      <TouchableOpacity>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
